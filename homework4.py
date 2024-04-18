9c0646c8c13b8fe1525f8ce85d18d69ae92eadd6
YaEd71 <166940263+YaEd71@users.noreply.github.com>
2024-04-18 12:09:33 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,9 @@
+immutable_tuple = 1, 2, 'a, b,'
+print (immutable_tuple)
+immutable_tuple_1 = (1, 2, 'a, b,')
+print (immutable_tuple_1)
+print (immutable_tuple[0:3:2])
+mutable_list = 1, 2, 'a, b,'
+print (mutable_list)
+mutable_list = 1, 2, 'vodified'
+print (mutable_list)
